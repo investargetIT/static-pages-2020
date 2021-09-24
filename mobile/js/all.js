de7ae7e1516efa6136a0471f89abb514ddfa6cd1
InvesTarget <system@investarget.com>
2021-09-24 11:41:32 -0300

    Finish mobile homepage

--- mobile/js/all.js
+++ mobile/js/all.js
@@ -713,7 +713,7 @@ $(function() {
     // 
     $('#layout').append(`<footer>
             <div class="fend_">
-                <p>copyright©2020 Investarget </p>
+                <p>copyright©2021 Investarget </p>
                 <p><a style="color: #999;" href="http://www.beian.miit.gov.cn/">沪ICP备15056238号-1</a></p>
             </div>
         </footer>`)
